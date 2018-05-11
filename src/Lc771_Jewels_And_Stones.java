f0eae79ee432de019d55264fc769abcb58cb33dc
Charles <liuchangbai@CharlesdeMacBook-Pro.local>
2018-05-11 00:30:03 -0400

    5.10

---
+++ src/Lc771_Jewels_And_Stones.java
@@ -0,0 +1,9 @@
+/**
+ * Input: A Number
+ * Output: Roman Number
+ * Tag: Hash Table, String, Math
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+public class Lc771_Jewels_And_Stones {
+}
--- src/Lc9_Palindrome_Number.java
+++ src/Lc9_Palindrome_Number.java
@@ -2,9 +2,10 @@
  * Input: A number
  * Output: Is it Palindrome or not?
  * Tag: Math
- * Time Complexity:
- * Space Complexity:
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
  */
+
 public class Lc9_Palindrome_Number {
     public boolean isPalindrome(int x) {
         if (x < 0 || x > 0 && x % 10 == 0) {
