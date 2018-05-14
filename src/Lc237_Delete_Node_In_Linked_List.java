5a321b349bb1b37d47180411a1456795b2231cf2
Charles <liuchangbai@CharlesdeMacBook-Pro.local>
2018-05-14 15:37:54 -0400

    05/14

--- src/Lc237_Delete_Node_In_Linked_List.java
+++ src/Lc237_Delete_Node_In_Linked_List.java
@@ -1,2 +1,20 @@
+/**
+ * Tag: Linked List
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ */
+
 public class Lc237_Delete_Node_In_Linked_List {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
 }
+
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
