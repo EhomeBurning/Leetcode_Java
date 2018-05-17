53804eb67d99a8b186b1e1ad850bc25ce0f33887
Charles <liuchangbai@CharlesdeMacBook-Pro.local>
2018-05-17 00:15:30 -0400

    05/16

---
+++ src/Lc122_Best_Time_To_Buy_And_Sell_Stock_II.java
@@ -0,0 +1,20 @@
+/**
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+public class Lc122_Best_Time_To_Buy_And_Sell_Stock_II {
+    public int maxProfit(int[] prices) {
+        if (prices.length < 2 || prices == null) {
+            return 0;
+        }
+
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
+}
