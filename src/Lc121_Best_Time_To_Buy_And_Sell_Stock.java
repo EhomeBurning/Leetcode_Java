c74442d6735e3742d0422f53859145f3035fbc21
Charles <liuchangbai@CharlesdeMacBook-Pro.local>
2018-05-30 22:53:04 -0400

    05/30

---
+++ src/Lc121_Best_Time_To_Buy_And_Sell_Stock.java
@@ -0,0 +1,13 @@
+public class Lc121_Best_Time_To_Buy_And_Sell_Stock {
+    public int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+}
