dd08b1b12f61ed81debbb483a43dc10e03ba420f
Charles <changbai1224@gmail.com>
2018-08-17 11:12:49 -0700

    08/17

--- src/Lc237_Delete_Node_In_Linked_List.java
+++ src/Lc237_Delete_Node_In_Linked_List.java
@@ -2,6 +2,8 @@
  * Tag: Linked List
  * Time Complexity: O(1)
  * Space Complexity: O(1)
+ * Solution: copy node.next to node, and jump to node.next.next; 4->5->1->9, 4->1->1->9,4->1->9;
+ * Note: given the only access to the node that we wanna delete;
  */
 
 public class Lc237_Delete_Node_In_Linked_List {
