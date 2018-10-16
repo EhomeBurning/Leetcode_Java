b92c11e2d63cc4d35212776e40f620445a323cf4
EhomeBurning <cliu175@scilib-cow-mac223.library.ucsc.edu>
2018-10-16 14:33:16 -0700

    10/16

---
+++ src/AAA.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.util.*;
+
+
+public class AAA {
+
+    public static int helper(int a) {
+        a = 50;
+        return a;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
