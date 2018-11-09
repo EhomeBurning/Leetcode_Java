fdd2ab7190983320ed0ba3a10999d1b1fe215bda
cliu175 <changbai1224@gmail.com>
2018-11-08 16:39:54 -0800

    11/09

--- src/Lc20_Valid_Parentheses.java
+++ src/Lc20_Valid_Parentheses.java
@@ -1,29 +1,42 @@
-import java.util.Stack;
-/**
- * Tag: String, Stack
- * Time Complexity: O(n)
- * Space Complexity: O(n)
- */
-public class Lc20_Valid_Parentheses {
+
+// Solution:Stack, 由于左右必须挨着，左边都push进去，遇到第一个右边的，pop出来一定是左边
+// Time: O(n)
+// space: O(n)
+
+
+class Solution {
     public boolean isValid(String s) {
-        if (s == null || s.length() == 0) return true;
-
-        Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                stack.push(')');
-            } else if (c == '[') {
-                stack.push(']');
-            } else if (c == '{') {
-                stack.push('}');
-            } else{
-                if (stack.isEmpty() || stack.pop() != c){
-                    return false;
-                }
-            }
-
+        if (s == null || s.length() == 0) {
+            return true;
         }
-        return stack.isEmpty();
-
+        Stack<Character> stack = new Stack<>();
+        for (Character ch : s.toCharArray()) {
+            switch (ch) {
+                case '}' : 
+                    if (stack.isEmpty() || stack.pop() != '{') {
+                        return false;
+                    } else {
+                        break;
+                    }
+                case ')' : 
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        return false;
+                    } else {
+                        break;
+                    }
+                case ']' : 
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return false;
+                    } else {
+                        break;
+                    }
+                default: 
+                    stack.push(ch);
+            }
+        }
+        if (!stack.isEmpty()) {
+            return false;
+        }
+        return true;
     }
 }
